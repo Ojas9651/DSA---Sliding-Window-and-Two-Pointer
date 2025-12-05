3db642fb4143e11240cb5fc04009efc3f535becf
Ojas Maurya <ojasmaurya30@gmail.com>
2025-12-06 02:47:45 +0530

    Notes and Code

--- Maximum_Consecutive_Ones_3.java
+++ Maximum_Consecutive_Ones_3.java
@@ -1,3 +1,14 @@
+/*
+    Better Solution -
+        Outer Loop (0 -> N) whenever zero > k move left till zero <= k
+        TC - O(2N), SC - O(1)
+    
+    Optimal Solution -
+        Loop (0 -> N)
+        if(zero <= k) move right and save max
+        else only move left and right do not save max
+*/
+
 public class Maximum_Consecutive_Ones_3 {
     public static int longestOnes(int[] nums, int k) {
         if(nums.length < 1) return 0;
